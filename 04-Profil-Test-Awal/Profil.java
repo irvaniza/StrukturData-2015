b5726591861326d34274e33da0de46b768943c86
irvaniza patria <irvaniza.patria@s1.informatika.unsyiah.ac.id>
2015-11-06 14:41:15 +0700

    04-Profil-Test-Awal

---
+++ 04-Profil-Test-Awal/Profil.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class Profil here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Profil
+{
+    private String nama="Irvaniza Patria";
+    private String nim="1408107010033";
+
+    public Profil ()
+    {
+        
+    }
+    
+    public Profil (String nama, String nim)
+    {
+        this.nama=nama;
+        this.nim=nim;
+    }
+    
+    public String getNama()
+    {
+        return nama;
+    }
+    
+    public String getNim()
+    {
+        return nim;
+    }
+}
